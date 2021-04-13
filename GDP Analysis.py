0e1660e7454e8437c3adb215fe3be7a7cb392aa9
Praffulla Kumar Dubey <praffullakrdubey@gmail.com>
2021-04-13 13:50:51 +0530

    added linear regression and random forest

--- GDP Analysis.py
+++ GDP Analysis.py
@@ -11,4 +11,118 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_squared_log_error
 
 data=pd.read_csv('countries of the world.csv',decimal=',')
-data.head()
+print(data.head())
+
+print(data.isnull().sum())
+
+print(data.groupby('Region')[['GDP ($ per capita)','Literacy (%)','Agriculture']].median())
+
+for col in data.columns.values:
+    if data[col].isnull().sum() == 0:
+        continue
+    if col == 'Climate':
+        guess_values = data.groupby('Region')['Climate'].apply(lambda x: x.mode().max())
+    else:
+        guess_values = data.groupby('Region')[col].median()
+    for region in data['Region'].unique():
+        data[col].loc[(data[col].isnull())&(data['Region']==region)] = guess_values[region]
+        
+
+fig, ax = plt.subplots(figsize=(16,6))
+#ax = fig.add_subplot(111)
+top_gdp_countries = data.sort_values('GDP ($ per capita)',ascending=False).head(20)
+mean = pd.DataFrame({'Country':['World mean'], 'GDP ($ per capita)':[data['GDP ($ per capita)'].mean()]})
+gdps = pd.concat([top_gdp_countries[['Country','GDP ($ per capita)']],mean],ignore_index=True)
+
+sns.barplot(x='Country',y='GDP ($ per capita)',data=gdps, palette='Set3')
+ax.set_xlabel(ax.get_xlabel(),labelpad=15)
+ax.set_ylabel(ax.get_ylabel(),labelpad=30)
+ax.xaxis.label.set_fontsize(16)
+ax.yaxis.label.set_fontsize(16)
+plt.xticks(rotation=90)
+plt.show()
+
+#heatmap
+plt.figure(figsize=(16,12))
+sns.heatmap(data=data.iloc[:,2:].corr(),annot=True,fmt='.2f',cmap='icefire')
+plt.show()
+
+
+#Top factors affecting GDP per capita
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(20,12))
+plt.subplots_adjust(hspace=0.4)
+
+corr_to_gdp = pd.Series()
+for col in data.columns.values[2:]:
+    if ((col!='GDP ($ per capita)')&(col!='Climate')):
+        corr_to_gdp[col] = data['GDP ($ per capita)'].corr(data[col])
+abs_corr_to_gdp = corr_to_gdp.abs().sort_values(ascending=False)
+corr_to_gdp = corr_to_gdp.loc[abs_corr_to_gdp.index]
+
+for i in range(2):
+    for j in range(3):
+        sns.regplot(x=corr_to_gdp.index.values[i*3+j], y='GDP ($ per capita)', data=data,
+                   ax=axes[i,j], fit_reg=False, marker='.')
+        title = 'correlation='+str(corr_to_gdp[i*3+j])
+        axes[i,j].set_title(title)
+axes[1,2].set_xlim(0,102)
+plt.show()
+
+#Countries with low birthrate and low GDP per capita
+print(data.loc[(data['Birthrate']<14)&(data['GDP ($ per capita)']<10000)])
+
+#Training and testing
+LE = LabelEncoder()
+data['Region_label'] = LE.fit_transform(data['Region'])
+data['Climate_label'] = LE.fit_transform(data['Climate'])
+print(data.head())
+
+train, test = train_test_split(data, test_size=0.3, shuffle=True)
+training_features = ['Population', 'Area (sq. mi.)',
+       'Pop. Density (per sq. mi.)', 'Coastline (coast/area ratio)',
+       'Net migration', 'Infant mortality (per 1000 births)',
+       'Literacy (%)', 'Phones (per 1000)',
+       'Arable (%)', 'Crops (%)', 'Other (%)', 'Birthrate',
+       'Deathrate', 'Agriculture', 'Industry', 'Service', 'Region_label',
+       'Climate_label','Service']
+target = 'GDP ($ per capita)'
+train_X = train[training_features]
+train_Y = train[target]
+test_X = test[training_features]
+test_Y = test[target]
+
+#linear regression
+model = LinearRegression()
+model.fit(train_X, train_Y)
+train_pred_Y = model.predict(train_X)
+test_pred_Y = model.predict(test_X)
+train_pred_Y = pd.Series(train_pred_Y.clip(0, train_pred_Y.max()), index=train_Y.index)
+test_pred_Y = pd.Series(test_pred_Y.clip(0, test_pred_Y.max()), index=test_Y.index)
+
+rmse_train = np.sqrt(mean_squared_error(train_pred_Y, train_Y))
+msle_train = mean_squared_log_error(train_pred_Y, train_Y)
+rmse_test = np.sqrt(mean_squared_error(test_pred_Y, test_Y))
+msle_test = mean_squared_log_error(test_pred_Y, test_Y)
+
+print('rmse_train:',rmse_train,'msle_train:',msle_train)
+print('rmse_test:',rmse_test,'msle_test:',msle_test)
+
+#random forest
+model = RandomForestRegressor(n_estimators = 50,
+                             max_depth = 6,
+                             min_weight_fraction_leaf = 0.05,
+                             max_features = 0.8,
+                             random_state = 42)
+model.fit(train_X, train_Y)
+train_pred_Y = model.predict(train_X)
+test_pred_Y = model.predict(test_X)
+train_pred_Y = pd.Series(train_pred_Y.clip(0, train_pred_Y.max()), index=train_Y.index)
+test_pred_Y = pd.Series(test_pred_Y.clip(0, test_pred_Y.max()), index=test_Y.index)
+
+rmse_train = np.sqrt(mean_squared_error(train_pred_Y, train_Y))
+msle_train = mean_squared_log_error(train_pred_Y, train_Y)
+rmse_test = np.sqrt(mean_squared_error(test_pred_Y, test_Y))
+msle_test = mean_squared_log_error(test_pred_Y, test_Y)
+
+print('rmse_train:',rmse_train,'msle_train:',msle_train)
+print('rmse_test:',rmse_test,'msle_test:',msle_test)
